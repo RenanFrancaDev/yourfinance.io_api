f5241ae3e69fca9b39ba424392d3b721a594c4d9
Renan Franca <renanfranca.dev@gmail.com>
2024-01-09 19:23:06 -0300

    feat: user model created

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,17 @@
+import knex from "../../services/knex";
+
+export const getAll = () => {
+  return knex("users");
+};
+
+export const save = (params) => {
+  return knex("users").insert(params);
+};
+
+export const remove = (id) => {
+  return knex("users").remove(id);
+};
+
+export const update = (id, params) => {
+  return knex("users").where({ id }).update(params);
+};
