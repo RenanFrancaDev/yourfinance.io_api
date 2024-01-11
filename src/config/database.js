748a1622507b0b0d4fa0b4a040c79ad24954efb7
Renan Franca <renanfranca.dev@gmail.com>
2024-01-11 11:02:52 -0300

    feat: database connect created

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+import dotenv, { config } from "dotenv";
+dotenv.config();
+
+const knexConfig = {
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST || "localhost",
+    port: 3306,
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || "yourfinance",
+  },
+};
+
+export default knexConfig;
