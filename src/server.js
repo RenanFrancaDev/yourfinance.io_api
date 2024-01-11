0d97498b72bfadf702638ebed67d4181864a6b8c
Renan Franca <renanfranca.dev@gmail.com>
2024-01-11 11:11:09 -0300

    chore: ensure that server is connected with database

--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import knex from "./config/database.js";
+dotenv.config();
 import userRoute from "./modules/user/user.route.js";
 const app = express();
 app.use(express.json());
 
-app.use("user", userRoute);
+app.use("/users", userRoute);
 
 app.get("/", (_, res) => {
   return res.send("Sistem Working");
 });
 
-app.listen(4000, () => {
+app.listen(4000, async () => {
+  const result = await knex("users");
+  console.log("result", result);
   console.log("Server working in PORT 4000");
 });
