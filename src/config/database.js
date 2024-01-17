a75ff8ca382bb491e8dcbc7452843f40d2f5d3f3
Renan Franca <renanfranca.dev@gmail.com>
2024-01-16 22:44:36 -0300

    chore: bearer token package working

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const knexConfig = {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST || "localhost",
     port: 3306,
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "Panmetal@2020",
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || "yourfinance",
   },
 };
--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
-import knex from "./services/knex.js";
 dotenv.config();
+import cors from "cors";
+import bearerToken from "express-bearer-token";
+import knex from "./services/knex.js";
 import userRoute from "./modules/user/user.route.js";
 import authRoute from "./modules/auth/auth.route.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(bearerToken());
 
 app.use("/users", userRoute);
 app.use("/auth", authRoute);
