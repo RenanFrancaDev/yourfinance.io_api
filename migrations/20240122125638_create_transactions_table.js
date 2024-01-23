5a3ac100fb456c6599711beb7146832ef743fed4
Renan Franca <renanfranca.dev@gmail.com>
2024-01-23 11:18:13 -0300

    feat: transaction migration created

---
+++ migrations/20240122125638_create_transactions_table.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function (knex) {
+  return knex.schema.createTable("transactions", (table) => {
+    table.bigIncrements("id");
+    table.string("description");
+    table.integer("amount");
+    table.date("date");
+    table.string("type");
+    table
+      .bigint("category_id")
+      .unsigned()
+      .references("id")
+      .inTable("categories");
+    table.bigint("user_id").unsigned().references("id").inTable("users");
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function (knex) {
+  return knex.schema.dropTable("transactions");
+};
